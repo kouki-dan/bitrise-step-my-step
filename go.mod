a70989964c0cb3e9e6946fe1fcfa2e16aa9c5e4b
Kouki Saito <dan.addr.skd@gmail.com>
2020-06-30 09:18:07 +0900

    Add yaml library

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/kouki-dan/bitrise-step-my-step
+
+go 1.13
+
+require (
+	github.com/ghodss/yaml v1.0.0
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,30 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/ghodss/yaml"
 	"os"
 	"os/exec"
 )
 
 func main() {
-	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
-
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
+	j := []byte(`{"yaml": "test", "yaml2": test2}`)
+	y, err := yaml.JSONToYAML(j)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "YAML_OUTPUT", "--value", string(y)).CombinedOutput()
 	if err != nil {
 		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
 		os.Exit(1)
 	}
-	// You can find more usage examples on envman's GitHub page
-	//  at: https://github.com/bitrise-io/envman
 
-	//
-	// --- Exit codes:
-	// The exit code of your Step is very important. If you return
-	//  with a 0 exit code `bitrise` will register your Step as "successful".
-	// Any non zero exit code will be registered as "failed" by `bitrise`.
 	os.Exit(0)
 }
